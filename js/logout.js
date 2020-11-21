e30ba747e19595ddd2f4b58d1130dfd108812286
Lucas S N Moreira <lucassng@live.com>
2020-11-21 14:43:02 -0300

    Create logout.js

---
+++ js/logout.js
@@ -0,0 +1,9 @@
+function logout(){
+    localStorage.removeItem('idLogin')
+    localStorage.removeItem('nomeLogin')
+    localStorage.removeItem('emailLogin')
+    localStorage.removeItem('cpfLogin')
+    localStorage.removeItem('pontuacaoLogin')
+
+    location.replace("./index.html")
+}
